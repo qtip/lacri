e73b9d80f7f5fe81b1e82bc2727e56eb5c56f905
Daniel Snider <qtipkk@gmail.com>
2015-07-10 20:37:12 -0700

    views: Convert views to Class-based
    This is just a first-pass conversion to class based views with no
    generic views at all

--- lacri/urls.py
+++ lacri/urls.py
@@ -4,49 +4,28 @@ import views
 
 urlpatterns = patterns('',
     # e.g. /
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
 
     # e.g. /user/alice/
-    url(r'^user/(?P<username>\w+)/$', views.user_detail, name='user_detail'),
-
-    # e.g. /user/alice/root/
-    #url(r'^user/(?P<username>\w+)/root/$', views.roots, name='roots'),
+    url(r'^user/(?P<username>\w+)/$', views.UserDetailView.as_view(), name='user_detail'),
 
     # e.g. /user/alice/root/my-main-ca.pem
-    url(r'^user/(?P<username>\w+)/root/(?P<root_slug>[\w_-]+).pem$', views.root_pem, name='root_pem'),
+    url(r'^user/(?P<username>\w+)/root/(?P<root_slug>[\w_-]+).(?P<ext>crt|pem)$', views.RootCertPemView.as_view(), name='root_cert_pem'),
+
+    # e.g. /user/alice/root/my-main-ca.der
+    url(r'^user/(?P<username>\w+)/root/(?P<root_slug>[\w_-]+).der$', views.RootCertDerView.as_view(), name='root_cert_der'),
 
     # e.g. /user/alice/root/my-main-ca/
-    url(r'^user/(?P<username>\w+)/root/(?P<root_slug>[\w_-]+)/$', views.root_detail, name='root_detail'),
-
-    # e.g. /user/alice/root/my-main-ca/domain/
-    #url(r'^user/(?P<username>\w+)/root/(?P<root_slug>[\w_-]+)/domain/$', views.domains, name='domains'),
+    url(r'^user/(?P<username>\w+)/root/(?P<root_slug>[\w_-]+)/$', views.RootDetailView.as_view(), name='root_detail'),
 
     # e.g. /user/alice/root/my-main-ca/domain/*.alice.com.tar
-    url(r'^user/(?P<username>\w+)/root/(?P<root_slug>[\w_-]+)/domain/(?P<domain>(\*\.)?[\w\-\.]*[\w\-]).tar$', views.domain_tar, name='domain_tar'),
+    url(r'^user/(?P<username>\w+)/root/(?P<root_slug>[\w_-]+)/domain/(?P<domain>(\*\.)?[\w\-\.]*[\w\-]).tar$', views.DomainTarView.as_view(), name='domain_tar'),
 
     # e.g. /user/alice/root/my-main-ca/domain/*.alice.com.crt
-    url(r'^user/(?P<username>\w+)/root/(?P<root_slug>[\w_-]+)/domain/(?P<domain>(\*\.)?[\w\-\.]*[\w\-]).crt$', views.domain_cert, name='domain_cert'),
+    url(r'^user/(?P<username>\w+)/root/(?P<root_slug>[\w_-]+)/domain/(?P<domain>(\*\.)?[\w\-\.]*[\w\-]).(?P<ext>crt|pem)$', views.DomainCertPemView.as_view(), name='domain_cert_pem'),
 
     # e.g. /user/alice/root/my-main-ca/domain/*.alice.com
-    url(r'^user/(?P<username>\w+)/root/(?P<root_slug>[\w_-]+)/domain/(?P<domain>(\*\.)?[\w\-\.]*[\w\-])$', views.domain_detail, name='domain_detail'),
+    url(r'^user/(?P<username>\w+)/root/(?P<root_slug>[\w_-]+)/domain/(?P<domain>(\*\.)?[\w\-\.]*[\w\-])$', views.DomainDetailView.as_view(), name='domain_detail'),
 
-
-
-    # client certs
-    # ------------
-
-    # e.g. /user/alice/root/*.alice.com/client/
-
-    # e.g. /user/alice/root/*.alice.com/client/bob
-
-    # e.g. /user/alice/root/*.alice.com/client/bob.crt
-
-    # OpenVPN config
-    # --------------
-
-    # e.g. /user/alice/root/*.alice.com/ovpn/
-
-    # e.g. /user/alice/root/*.alice.com/ovpn/config.tar
-
-    # e.g. /user/alice/root/*.alice.com/client/bob/ovpn/bob-desktop.ovpn
 )
+
--- lacri/views.py
+++ lacri/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login
 from django.db import IntegrityError
+from django.views.generic import View
 
 from django.contrib.sites.models import Site
 from django.contrib.auth.models import User
@@ -14,8 +15,11 @@ from .forms import CreateUserForm, CreateRootForm, CreateDomainForm
 
 logger = logging.getLogger('lacri')
 
-def index(request):
-    if request.method == "POST":
+class IndexView(View):
+    def get(self, request):
+        return render(request, "lacri/index.html", {"form": CreateUserForm()})
+
+    def post(self, request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data['username']
@@ -38,76 +42,84 @@ def index(request):
                     return HttpResponseRedirect(reverse('user_detail', kwargs={'username': username}))
                 except IntegrityError:
                     form.add_error('username', 'Incorrect username/password')
-    else: # GET
-        form = CreateUserForm()
-    return render(request, "lacri/index.html", {"form": form})
+        return render(request, "lacri/index.html", {"form": form})
 
-def user_detail(request, username):
-    user = get_object_or_404(User, username=username)
-    if request.method == 'POST':
+class UserDetailView(View):
+    def get(self, request, username):
+        user = get_object_or_404(User, username=username)
+        roots = user.authority_set.filter(usage=Authority.ROOT)
+        return render(request, "lacri/user_detail.html", {'user':user, 'roots':roots, 'form': CreateRootForm()})
+
+    def post(self, request, username):
+        user = get_object_or_404(User, username=username)
+        roots = user.authority_set.filter(usage=Authority.ROOT)
         form = CreateRootForm(request.POST)
         if form.is_valid():
             common_name = form.cleaned_data['common_name']
             root = Authority(user=user, common_name=common_name, usage=Authority.ROOT)
             root.save()
             return HttpResponseRedirect(reverse('root_detail', kwargs={'username': username, 'root_slug': root.slug}))
-    else:
-        form = CreateRootForm()
-    roots = user.authority_set.filter(usage=Authority.ROOT)
-    return render(request, "lacri/user_detail.html", {'user':user, 'roots':roots, 'form': form})
+        roots = user.authority_set.filter(usage=Authority.ROOT)
+        return render(request, "lacri/user_detail.html", {'user':user, 'roots':roots, 'form': form})
 
-#def roots(request, username):
-    #user = get_object_or_404(User, username=username)
-    #roots = user.authority_set.filter(usage=Authority.ROOT)
-    #return render(request, "lacri/roots.html", {'roots':roots})
+class RootDetailView(View):
+    def get(self, request, username, root_slug):
+        user = get_object_or_404(User, username=username)
+        root = get_object_or_404(Authority, user__username=username, slug=root_slug)
+        site = Site.objects.get_current()
+        domains = root.authority_set.filter(usage=Authority.DOMAIN)
+        form = CreateDomainForm()
+        return render(request, "lacri/root_detail.html", {'request': request, 'site': site, 'user':user, 'root': root, 'domains':domains, 'form': form})
 
-def root_detail(request, username, root_slug):
-    user = get_object_or_404(User, username=username)
-    root = get_object_or_404(Authority, user__username=username, slug=root_slug)
-    if request.method == 'POST':
+    def post(self, request, username, root_slug):
+        user = get_object_or_404(User, username=username)
+        root = get_object_or_404(Authority, user__username=username, slug=root_slug)
+        site = Site.objects.get_current()
+        domains = root.authority_set.filter(usage=Authority.DOMAIN)
         form = CreateDomainForm(request.POST)
         if form.is_valid():
             common_name = form.cleaned_data['common_name']
             domain = Authority(user=user, common_name=common_name, parent=root, usage=Authority.DOMAIN)
             domain.save()
             return HttpResponseRedirect(reverse('domain_detail', kwargs={'username': username, 'root_slug': root.slug, 'domain': domain.common_name}))
-    else:
-        form = CreateDomainForm()
-    site = Site.objects.get_current()
-    domains = root.authority_set.filter(usage=Authority.DOMAIN)
-    return render(request, "lacri/root_detail.html", {'request': request, 'site': site, 'user':user, 'root': root, 'domains':domains, 'form': form})
+        return render(request, "lacri/root_detail.html", {'request': request, 'site': site, 'user':user, 'root': root, 'domains':domains, 'form': form})
 
-def root_pem(request, username, root_slug):
-    user = get_object_or_404(User, username=username)
-    root = get_object_or_404(Authority, user__username=username, slug=root_slug)
-    response = HttpResponse(content_type="application/x-pem-file")
-    response.write(root.cert)
-    return response
+class RootCertPemView(View):
+    def get(self, request, username, root_slug, ext):
+        user = get_object_or_404(User, username=username)
+        root = get_object_or_404(Authority, user__username=username, slug=root_slug)
+        response = HttpResponse(content_type="application/x-pem-file")
+        response.write(root.cert)
+        return response
 
+class RootCertDerView(View):
+    def get(self, request, username, root_slug):
+        content_type="application/x-x509-ca-cert"
+        raise NotImplementedError()
 
-#def domains(request, username, root_slug):
-    #return HttpResponse("ohai user " + username + ", here're your domains for " + root_slug)
+class DomainDetailView(View):
+    def get(self, request, username, root_slug, domain):
+        site = Site.objects.get_current()
+        user = get_object_or_404(User, username=username)
+        root = get_object_or_404(Authority, user__username=username, slug=root_slug)
+        domain = root.authority_set.filter(usage=Authority.DOMAIN, parent=root, common_name=domain).get()
+        return render(request, "lacri/domain_detail.html", {'request': request, 'site': site, 'user':user, 'root': root, 'domain':domain})
 
-def domain_detail(request, username, root_slug, domain):
-    site = Site.objects.get_current()
-    user = get_object_or_404(User, username=username)
-    root = get_object_or_404(Authority, user__username=username, slug=root_slug)
-    domain = root.authority_set.filter(usage=Authority.DOMAIN, parent=root, common_name=domain).get()
-    return render(request, "lacri/domain_detail.html", {'request': request, 'site': site, 'user':user, 'root': root, 'domain':domain})
+class DomainCertPemView(View):
+    def get(self, request, username, root_slug, domain, ext):
+        user = get_object_or_404(User, username=username)
+        root = get_object_or_404(Authority, user__username=username, slug=root_slug)
+        domain = root.authority_set.filter(usage=Authority.DOMAIN, parent=root, common_name=domain).get()
+        response = HttpResponse(content_type="application/x-pem-file")
+        response.write(domain.cert)
+        return response
 
-def domain_cert(request, username, root_slug, domain):
-    return "asdf"
+class DomainTarView(View):
+    def get(self, request, username, root_slug, domain):
+        user = get_object_or_404(User, username=username)
+        root = get_object_or_404(Authority, user__username=username, slug=root_slug)
+        domain = root.authority_set.filter(usage=Authority.DOMAIN, parent=root, common_name=domain).get()
+        response = HttpResponse(content_type="application/x-tar")
+        domain.write_tar(response)
+        return response
 
-def domain_tar(request, username, root_slug, domain):
-    user = get_object_or_404(User, username=username)
-    root = get_object_or_404(Authority, user__username=username, slug=root_slug)
-    domain = root.authority_set.filter(usage=Authority.DOMAIN, parent=root, common_name=domain).get()
-    response = HttpResponse(content_type="application/x-tar")
-    domain.write_tar(response)
-    return response
-
-#def client_ovpn(request, username, network_slug, client_slug):
-    #client = get_object_or_404(Client, network__user__username=username, network__slug=network_slug, slug=client_slug)
-    #response = HttpResponse(content_type="application/octet-stream")
-    #response.write(client.ovpn_conf())
-    #return response
